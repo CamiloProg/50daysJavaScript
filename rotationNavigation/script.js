12f1318afab824cd72e9fa57e333cbfb17a0bc0f
CamiloProg <camilom200107@gmail.com>
2023-11-12 19:02:11 -0500

    RotationNavigation

---
+++ rotationNavigation/script.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById('menu');
+const container = document.getElementById('container');
+const x = document.getElementById('x');
+const burguer = document.getElementById('burguer');
+function rotate() {
+  container.classList.toggle('rotated');
+  menu.classList.toggle('rotatedMenu')
+}
